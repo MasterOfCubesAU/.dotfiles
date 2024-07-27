53f896d766d49d0d757fedde04dd6e363a74f0b3
MasterOfCubesAU <38149391+MasterOfCubesAU@users.noreply.github.com>
2024-07-27 15:05:55 +1000

    fix: backup dirs now are created properly

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -8,15 +8,16 @@ git clone --bare git@github.com:MasterOfCubesAU/.dotfiles.git $DOTFILES_DIR
 function dotfiles {
    /usr/bin/git --git-dir=$DOTFILES_DIR --work-tree=$HOME $@
 }
-mkdir -p $DOTFILES_BACKUP_DIR
-dotfiles checkout
-if [ $? = 0 ]; then
-  echo "Applied dotfiles";
-  else
+
+# Back up existing dotfiles if necessary
+if ! dotfiles checkout > /dev/null; then
     echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\..*/" | awk {'print $1'} | xargs -I{} dirname {} | xargs -I{} mkdir -p $DOTFILES_BACKUP_DIR/{}
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $DOTFILES_BACKUP_DIR/{}
+    dotfiles checkout
 fi;
-dotfiles checkout
+
+echo "Applied dotfiles";
 dotfiles config status.showUntrackedFiles no
 
 rm -- "$HOME/README.md" "$0" 
